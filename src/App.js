fe1a6d18cd7ef66e0cbf72b2b84f1131445d6dd6
Mohammed Aftab Hussain <73380320+aftab2001@users.noreply.github.com>
2023-03-09 21:48:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,11 +15,14 @@ const movie1={
 const App = () => {
     const[movies,setMovies]=useState([]);
     const [searchTerm,setSearchTerm]=useState('');
-    const searchMovies= async(title)=>{
-        const response=await fetch(`${API_URL}&s=${title}`);
-        const data=await response.json();
-        setMovies(data.Search);
-    }
+   const searchMovies = async (title) => {
+const response = await fetch(${API_URL}&s=${title});
+const data = response.json();
+console.log(data)
+data.then((res) => {
+setMovies(res.Search);
+});
+}
     useEffect(()=>{
         searchMovies('Spiderman');
 
